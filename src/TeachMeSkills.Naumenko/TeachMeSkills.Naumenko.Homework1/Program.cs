55e01dd2c9365a4e639b62157216244e1021d877
evilgodhedgehog <evilgodhedgehog@gmail.com>
2021-09-23 22:09:23 +0300

    feat: Initial commit

---
+++ src/TeachMeSkills.Naumenko/TeachMeSkills.Naumenko.Homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.Naumenko.Homework1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dmitry!");
+        }
+    }
+}
