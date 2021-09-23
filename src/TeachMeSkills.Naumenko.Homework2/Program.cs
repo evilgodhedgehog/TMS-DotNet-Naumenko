3745bc4185d103da9dcad01ed0389a9602ebf9ad
evilgodhedgehog <evilgodhedgehog@gmail.com>
2021-09-23 22:39:30 +0300

    feat: Create program handler days

--- src/TeachMeSkills.Naumenko.Homework2/Program.cs
+++ src/TeachMeSkills.Naumenko.Homework2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TeachMeSkills.Naumenko.Homework2
 {
@@ -6,7 +7,20 @@ namespace TeachMeSkills.Naumenko.Homework2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите дату в формате (число, месяц, год), например: 23.09.2021");
+            string dateAdd = Console.ReadLine();
+            string result;
+            DateTime day;
+
+            if (DateTime.TryParse(dateAdd, out day))
+            {
+                result = day.ToString("dddd", CultureInfo.GetCultureInfo("ru-RU"));
+                Console.WriteLine($"{result}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректное значение. Попробуйте ещё раз.");
+            }
         }
     }
 }
