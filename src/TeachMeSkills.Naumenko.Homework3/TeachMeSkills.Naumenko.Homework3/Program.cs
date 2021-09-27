322bdb44319a127ccb626c0b77458b5989d920df
evilgodhedgehog <evilgodhedgehog@gmail.com>
2021-09-27 21:51:21 +0300

    init: create project handler day (methods))

---
+++ src/TeachMeSkills.Naumenko.Homework3/TeachMeSkills.Naumenko.Homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.Naumenko.Homework3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
