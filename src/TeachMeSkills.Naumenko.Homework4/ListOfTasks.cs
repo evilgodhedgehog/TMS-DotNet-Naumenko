9b1128995d92849bc1eb3fd8f2009035d9f83a2a
evilgodhedgehog <evilgodhedgehog@gmail.com>
2021-10-07 18:22:40 +0300

    feat: Create methods(input, output, input on/off))

---
+++ src/TeachMeSkills.Naumenko.Homework4/ListOfTasks.cs
@@ -0,0 +1,23 @@
+﻿namespace TeachMeSkills.Naumenko.Homework4
+{
+    internal class ListOfTasks
+    {
+        DateTime DateTimeTask = DateTime.Now;
+        string Id = Guid.NewGuid().ToString().ToUpper().Substring(0, 3);
+        
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public void TaskDisplay()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Status: {Status}");
+            Console.WriteLine($"Date of creation: {DateTimeTask}");
+        }
+    }
+}
--- src/TeachMeSkills.Naumenko.Homework4/Program.cs
+++ src/TeachMeSkills.Naumenko.Homework4/Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TeachMeSkills.Naumenko.Homework4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<ListOfTasks> listOfTasks = new List<ListOfTasks>();
+            InputTask(listOfTasks);
+            ShowTasks(listOfTasks);
+        }
+
+        static void InputTask(List<ListOfTasks> listOfTasks)
+        {
+            bool stopInput = false;
+
+            do
+            {
+                Console.Write("Input name: ");
+                var name = Console.ReadLine();
+                Console.Write("Input description: ");
+                var description = Console.ReadLine();
+                Console.Write("Input status: ");
+                var status = Console.ReadLine();
+
+                var task = new ListOfTasks
+                {
+                    Name = name,
+                    Description = description,
+                    Status = status,
+                };
+
+                listOfTasks.Add(task);
+
+
+                stopInput = StopInput();
+
+
+            } while (!stopInput);
+        }
+
+        static bool StopInput()
+        {
+            Console.Write("Stop input? (Press Y/y (Yes) or N/n (No)): ");
+            var key = ConsoleKey.N;
+            var stopInput = false;
+
+            while (key != ConsoleKey.Y)
+            {
+                key = Console.ReadKey().Key;
+                Console.WriteLine("\n");
+
+                if (key == ConsoleKey.Y)
+                {
+                    stopInput = true;
+                }
+                else if (key == ConsoleKey.N)
+                {
+                    stopInput = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect value. Press Y/y (Yes) or N/n (No) ");
+                }
+            }
+
+            return stopInput;
+        }
+
+        static void ShowTasks(List<ListOfTasks> listOfTasks)
+        {
+            foreach (var task in listOfTasks)
+            {
+                task.TaskDisplay();
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
+
 
